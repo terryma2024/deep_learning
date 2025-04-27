6546c967174402724926c16c72e73d06192dcfc4
terry.ma <terry.ma@bytedance.com>
2025-04-27 19:29:24 +0800

    add more print to identify if it's a remote ide

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Hello World")
+
+import os
+
+current_directory = os.getcwd()
+items = os.listdir(current_directory)
+files = [item for item in items if os.path.isfile(os.path.join(current_directory, item))]
+for file in files:
+    print(file)
